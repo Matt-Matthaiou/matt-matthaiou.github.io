c5c80e8e3bc625fd9313a833a89dd7cf867f848c
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-05-18 10:08:09 +0100

    a little progress on positioning items

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,16 +3,26 @@ import React from "react";
 const Intro = ()=>
 {
     return(
-        <div>
-            <h2>Hello, I am <b>Matthaios Matthaiou</b></h2>
-            <h3>Software Developer</h3>
-            <p>A newly trained software developer based in Edinburgh. A hardworking
-            professional individual with an extensive experience in customer
-            service. Adaptable team player with the willingness to learn and even
-            teach newly acquired skills.</p>
-            <a href="https://www.facebook.com/manos.matheou"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" width="40px" height="40px"></img></a>
-            <a href="https://github.com/Matt-Matthaiou"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="40px" height="40px"></img></a>
-            <a href="https://www.linkedin.com/in/matthaios-matthaiou-052763134/?fbclid=IwAR3L1GXxdMcT7flT9DFjT14LXj5pQE3tGhwmovNF_QP0MBep613ZjyrwMxY"><img src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1652806493~hmac=056b7f5b23996450a89f6d2608c0e934" width="40px" height="40px"></img></a>
+        <div id="intro-container">
+            <div id="intro">
+                <h2>Hello, I am <b>Matthaios Matthaiou</b></h2>
+                <h3>Software Developer</h3>
+            </div>
+            <div>
+                <div id="box">
+                    <h4>About me</h4>
+                    <p>A newly trained software developer based in Edinburgh. A hardworking
+                    professional individual with an extensive experience in customer
+                    service. Adaptable team player with the willingness to learn and even
+                    teach newly acquired skills.</p>
+                    <div>
+                <a href="https://www.facebook.com/manos.matheou"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" width="40px" height="40px"></img></a>
+                <a href="https://github.com/Matt-Matthaiou"><img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" width="40px" height="40px"></img></a>
+                <a href="https://www.linkedin.com/in/matthaios-matthaiou-052763134/?fbclid=IwAR3L1GXxdMcT7flT9DFjT14LXj5pQE3tGhwmovNF_QP0MBep613ZjyrwMxY"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="40px" height="40px"></img></a>
+            </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
